b237301874088d71a078e4cc4de1573209b7492d
codegaws <codegaws@gmail.com>
2023-12-20 15:48:19 -0500

    Fecha20-12-2023

--- Notes.sql
+++ Notes.sql
@@ -1,1 +1,19 @@
 // notas Scrip SQL
+CREATE DATABASE ADMINISTRACION;
+	drop database administracion;
+    create database administracion;
+    show databases;
+    show tables;
+    create table usuarios(
+		nombre varchar(30),
+        clase varchar(10)
+    )
+    describe usuarios;
+    drop table usuarios;
+    insert into usuarios(nombre,clase) values('Mario Perez','Marito');
+	select nombre,clase from usuarios;
+
+
+    //EJERCICIO
+
+    
