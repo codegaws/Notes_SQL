d7bf064e93af7ddc9bfe1af52809995145767ba3
codegaws <codegaws@gmail.com>
2023-12-27 14:56:34 -0500

    TRABAJO POR LA TARDE SQL

--- Notes.sql
+++ Notes.sql
@@ -200,6 +200,5 @@ También, los libros cuyo precio sea menor o igual a 30:
 
 select titulo,autor,editorial,precio from libros where precio<=30;*/
 
-/*EJERCICIO*/
-27-12-2023
+/*EJERCICIO FUENTE EN EL TRABAJO*/
 
