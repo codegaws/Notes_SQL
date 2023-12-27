be9166a4df01370b6e5779200f6b8236d514b003
codegaws <codegaws@gmail.com>
2023-12-27 16:59:31 -0500

    prueba44

--- Notes.sql
+++ Notes.sql
@@ -285,4 +285,30 @@ insert into usuarios (nombre, clave) values ('Marcelo','River');
 insert into usuarios (nombre, clave) values ('Gustavo','River');
 
 /*delete*/
-PRUEBA 2
+drop table if exists  usuarios;
+
+create table usuarios (
+  nombre varchar(30),
+  clave varchar(10)
+);
+
+insert into usuarios (nombre, clave) values ('Leonardo','payaso');
+insert into usuarios (nombre, clave) values ('MarioPerez','Marito');
+insert into usuarios (nombre, clave) values ('Marcelo','River');
+insert into usuarios (nombre, clave) values ('Gustavo','River');
+
+set SQL_SAFE_UPDATES=0;
+
+delete from usuarios;
+
+delete from usuarios where nombre='Leonardo';
+
+select nombre,clave from usuarios;
+
+delete from usuarios where clave='River';
+
+select nombre,clave from usuarios;
+
+set SQL_SAFE_UPDATES=1;
+
+select @@sql_safe_updates;
